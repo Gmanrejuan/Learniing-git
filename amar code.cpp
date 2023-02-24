13ceab13cd94da99d5ac2f15e901dd5ebfb660f6
Gmanrejuan <107629054+Gmanrejuan@users.noreply.github.com>
2023-02-24 15:44:27 +0600

    Add files via upload

---
+++ amar code.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fork(i, n) for(int i = 0; i < (int)n; i++)
+
+#define pb push_back
+
+#define all(ara) ara.begin(), ara.end()
+
+typedef long long ll;
+
+void solve(){
+
+    int n, i;
+
+    cin >> n;
+
+    vector<ll>ara(n, 0);
+
+    fork(i, n){
+
+        cin >> ara[i];
+    }
+}
+
+int main(){
+
+    int t = 1;
+
+    cin >> t;
+
+    while(t--){
+
+        solve();
+
+    }
+
+    return 0;
+}
